ee92c909a1e5eb6cdd3acfe263ae83446d439701
Liang <Zhaowei.Liang@austintexas.gov>
2018-07-17 14:13:57 -0500

    paste joshil's script into the flask framework
    Also cleaned up the project folder a little bit.

--- references/telnettest.py
+++ references/telnettest.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, request
-from IPINFO import *
+#Get and set libraries
 import getpass
 import sys
 import telnetlib
 import time
 
-def reset(HOST):
+#Chooses test controller IP address for testing
+HOST = "172.16.122.76"
+
+def main(HOST):
 
 
 	#password = getpass.getpass()
+
+
 	#sets up telnet
 	tn=telnetlib.Telnet(HOST, timeout=5)
 	#Default password for D4 telnets
@@ -31,33 +35,3 @@ def reset(HOST):
 	tn.write(command2.encode('ascii'))
 	#Ends telnet session
 	tn.close()
-
-
-app = Flask(__name__)
-
-@app.route('/')
-def my_form():
-	return render_template('home.html')
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-
-	ip = request.form["ip"]
-
-	if ip in IPINFO:
-		reset(ip)
-
-	else:
-		return ("Invalid IP address, please refresh the page and try again")
-
-@app.route('/confirmation')
-def confirmation():
-	return('contents on {ip} is about to be cleaned')
-
-if __name__ == "__main__":
-	app.run(debug = True)
-
-
-
-	# print(processed_text)
-
